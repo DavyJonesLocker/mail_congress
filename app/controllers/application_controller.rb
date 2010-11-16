350eaa85b80d3a0c08bb3e8b9bac56b19f2d6767
Brian Cardarella <bcardarella@gmail.com>
2010-11-16 04:57:53 -0500

    Locked the Gemfile and added http auth for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  private
+  
+  def authenticate
+    if Rails.env == 'production'
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "alpha" && password == "1234567890"
+      end
+    end
+  end
+
 end
