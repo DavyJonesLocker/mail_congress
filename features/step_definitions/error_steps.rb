b08c0d486acbdbb7d377dac497638d1194258a08
Brian Cardarella <bcardarella@gmail.com>
2010-11-15 21:08:15 -0500

    Better error finding

--- features/step_definitions/error_steps.rb
+++ features/step_definitions/error_steps.rb
@@ -1,5 +1,5 @@
-Then /^I should see the error "([^"]*)" for "([^"]*)"$/ do |error, label_text|
-  id = find(:xpath, XPath::HTML.fillable_field(label_text))['id']
+Then /^I should see the error "([^"]*)" for "([^"]*)"$/ do |error, locator|
+  id = find(:xpath, XPath::HTML.field(locator))['id']
   find(:xpath, "//*[@id='#{id}']/../span[@class='error']").text.should == error
 end
 
