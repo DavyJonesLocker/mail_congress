f2a27d16e4a51912a43453d1567b7758ee534540
Brian Cardarella <bcardarella@gmail.com>
2010-11-18 23:18:15 -0500

    Added a bunch of fictional addresses

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -45,7 +45,25 @@ class SearchController < ApplicationController
 
   def bogus_address?(address)
     bogus_addresses = [
-      '1640 Riverside Drive Hill Valley, CA'
+      '1640 Riverside Drive Hill Valley, CA',
+      '742 Evergreen Terrace Springfield, IM',
+      '344 Washington St. Boston, MA',
+      '185 West 74th Street, New York, NY',
+      '711 Calhoun Street, Chicago, IL',
+      '1060 West Addison Street, Chicago, IL',
+      '4222 Clinton Way, Los Angeles, CA',
+      '9764 Jeopardy Lane, Chicago, IL',
+      '1000 Mammon Lane, Springfield, IM',
+      '112 1/2 Beacon Street, Boston, MA',
+      '420 Paper St. Wilmington, DE',
+      '31 Spooner Street, Quahog, RI',
+      '11435 18th Avenue, Oahu, HI',
+      '2630 Hegal Place, Apt. 42, Alexandria, VA',
+      '129 West 81st Street Apt 5E New York, NY',
+      '88 Edgemont, Palisades, CA',
+      '633 Stag Trail Road North Caldwell, NJ',
+      '1630 Revello Drive, Sunnydale, CA',
+      '111 Archer Avenue, New York City, NY'
     ].include?(address)
   end
 
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -18,7 +18,25 @@ function placeHolder(input, text) {
 
 var Address = function() {
   var data = [
-    '1640 Riverside Drive Hill Valley, CA'
+    '1640 Riverside Drive Hill Valley, CA',
+    '742 Evergreen Terrace Springfield, IM',
+    '344 Washington St. Boston, MA',
+    '185 West 74th Street, New York, NY',
+    '711 Calhoun Street, Chicago, IL',
+    '1060 West Addison Street, Chicago, IL',
+    '4222 Clinton Way, Los Angeles, CA',
+    '9764 Jeopardy Lane, Chicago, IL',
+    '1000 Mammon Lane, Springfield, IM',
+    '112 1/2 Beacon Street, Boston, MA',
+    '420 Paper St. Wilmington, DE',
+    '31 Spooner Street, Quahog, RI',
+    '11435 18th Avenue, Oahu, HI',
+    '2630 Hegal Place, Apt. 42, Alexandria, VA',
+    '129 West 81st Street Apt 5E New York, NY',
+    '88 Edgemont, Palisades, CA',
+    '633 Stag Trail Road North Caldwell, NJ',
+    '1630 Revello Drive, Sunnydale, CA',
+    '111 Archer Avenue, New York City, NY'
   ];
   this.random = function() {
     var randomIndex = Math.ceil(Math.random()*data.length) - 1;
