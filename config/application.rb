cb7cef64d33d31dca31183f5fe91a79bca3c64ab
Brian Cardarella <bcardarella@gmail.com>
2010-11-23 14:27:54 -0500

    Filter password

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module MailCongress
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:number, :verification_value]
+    config.filter_parameters += [:number, :verification_value, :password, :password_confirmation]
     Haml::Template.options[:format] = :html5
   end
 end
