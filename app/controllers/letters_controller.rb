fb4ec831cbd8ae07e2a3e30646a2a82bd9f88eed
Brian Cardarella <bcardarella@gmail.com>
2010-12-07 10:55:00 -0500

    Save the letter

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -5,7 +5,6 @@ class LettersController < ApplicationController
   end
 
   def create
-    debugger
     letter = Letter.get_from_redis(params[:redis_key])
     paypal_response = Payment.complete(letter.cost, 
       :ip => request.headers["REMOTE_ADDR"],
@@ -13,6 +12,9 @@ class LettersController < ApplicationController
       :token => params[:token])
 
     if paypal_response.success?
+      letter.generate_follow_up_id!
+      letter.save
+      PrintJob.enqueue(letter)
       redirect_to thank_you_path
     else
       redirect_to destroy_letter_path(params[:redis_key]), :notice => paypal_response.message
