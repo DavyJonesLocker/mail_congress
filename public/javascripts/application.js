1fbdbd3905405054407474ef3008a599618cea68
Angelo Simeoni <angelo@cssboy.com>
2010-12-09 12:05:12 -0500

    added #flash to layout, modified style to center .flash so entire body doesn't shift.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 $(function() {
   $('input[placeholder], textarea[placeholder]').placeholder();
   $('label.placeheld').hide();
-  $('.alert, .notice').delay(3000).slideUp(500);
+  $('.alert, .notice').delay(3000).fadeOut(1000);
 
   $('li#sign_in a').click(function() { 
     $(this).hide(); 
